103258e14c8eebb067cfe45ab92bead4c643918a
Alican Kesen <46851204+kesenalican@users.noreply.github.com>
2022-10-18 09:21:01 +0300

    new queries addes

---
+++ WebAPI1/Controllers/DepoController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"SELECT
+                            dep_no,
+                            dep_adi
+                            FROM dbo.DEPOLAR WITH (NOLOCK)
+                            WHERE dep_no > 0
+                            UNION ALL
+                            SELECT
+                            CAST(0 AS integer),
+                            CAST(dbo.fn_GetResource('M',588, DEFAULT) AS nvarchar(50))
+                             ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DinamikMikroMobilConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
--- WebAPI1/Controllers/StoklarController.cs
+++ WebAPI1/Controllers/StoklarController.cs
@@ -15,15 +15,66 @@ namespace WebAPI1.Controllers
 
         }
         [HttpGet]
-        public JsonResult Get()
+        public JsonResult GetWithCode()
         {
-            string query = @"SELECT sto_kod AS StokKodu , sto_isim AS StokIsim, sfiyat_fiyati AS StokFiyat , sto_anagrup_kod AS StokAnaGrup, sto_sektor_kodu AS StokSektor,
-	sto_birim1_ad AS StokBirim1, sto_birim3_ad AS StokBirim3
-,sto_birim3_katsayi AS StokBirim3_katsayi, sto_reyon_kodu AS StokReyon, sto_marka_kodu AS StokMarka, sto_model_kodu AS StokModel
-FROM  STOKLAR 
-INNER JOIN STOK_SATIS_FIYAT_LISTELERI 
-ON STOKLAR.sto_kod = STOK_SATIS_FIYAT_LISTELERI.sfiyat_stokkod
- 
+            string query = @"SELECT TOP 100 PERCENT 
+                            sto_kod AS StokKodu /* KODU */,
+                            sto_isim AS StokIsim /* ADI */,
+                            ISNULL(sfiyat_fiyati,0) AS StokFiyat /* FİYAT */,
+                            Kur_adi AS StokKur /* DVZ */,
+                            sto_anagrup_kod AS StokAnaGrup,
+                            sto_sektor_kodu AS StokSektor,
+                            sto_birim1_ad AS StokBirim1, 
+                            sto_birim3_ad AS StokBirim3,
+                            sto_birim3_katsayi AS StokBirim3_katsayi,
+                            sto_reyon_kodu AS StokReyon,
+                            sto_marka_kodu AS StokMarka,
+                            sto_model_kodu AS StokModel,
+                            dbo.fn_DepodakiMiktar(sto_kod,1,0) AS [MERKEZ], 
+                            dbo.fn_EldekiMiktar(sto_kod) AS StokMiktar /* MİKTAR */ 
+                            FROM dbo.STOKLAR 
+                            LEFT OUTER JOIN dbo.STOK_SATIS_FIYATLARI_F1_D0_VIEW on sfiyat_stokkod = sto_kod AND sfiyat_satirno = 1 
+                            LEFT OUTER JOIN MikroDB_V16.dbo.KUR_ISIMLERI on Kur_No =  ISNULL(sfiyat_doviz,0)
+                            ORDER BY sto_kod 
+                             ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DinamikMikroMobilConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+        [HttpGet("orderbyName")]
+        public JsonResult GetWithName()
+        {
+            string query = @"SELECT TOP 100 PERCENT 
+                            sto_kod AS StokKodu /* KODU */,
+                            sto_isim AS StokIsim /* ADI */,
+                            ISNULL(sfiyat_fiyati,0) AS StokFiyat /* FİYAT */,
+                            Kur_adi AS StokKur /* DVZ */,
+                            dbo.fn_EldekiMiktar(sto_kod) AS StokMiktar /* MİKTAR */,
+                            sto_anagrup_kod AS StokAnaGrup,
+                            sto_sektor_kodu AS StokSektor,
+                            sto_birim1_ad AS StokBirim1,
+                            sto_birim3_ad AS StokBirim3,
+                            sto_birim3_katsayi AS StokBirim3_katsayi,
+                            sto_reyon_kodu AS StokReyon,
+                            sto_marka_kodu AS StokMarka,
+                            sto_model_kodu AS StokModel
+                            FROM STOKLAR WITH (NOLOCK)
+                            LEFT OUTER JOIN dbo.STOK_SATIS_FIYATLARI_F1_D0_VIEW on sfiyat_stokkod = sto_kod AND sfiyat_satirno = 1
+                            LEFT OUTER JOIN MikroDB_V16.dbo.KUR_ISIMLERI on Kur_No =  ISNULL(sfiyat_doviz,0)
+                            ORDER BY sto_isim
                              ";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("DinamikMikroMobilConn");
