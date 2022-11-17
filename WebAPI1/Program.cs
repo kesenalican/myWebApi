9d8b9ade5638ec78e5b3c68b49d035594c41842b
Alican Kesen <46851204+kesenalican@users.noreply.github.com>
2022-11-17 17:41:02 +0300

    new query added

--- WebAPI1/Program.cs
+++ WebAPI1/Program.cs
@@ -1,16 +1,10 @@
 using Newtonsoft.Json.Serialization;
-
-
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddCors(c =>
 {
     c.AddPolicy("AllowOrigin", options=>options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-}
-    );
-
+} );
 builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(
     options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
